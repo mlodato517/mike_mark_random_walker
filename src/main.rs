4404b8d5a299c27136f884c37d8c2c14da9c397e
Mark Lodato <mark@gnar.dog>
2020-03-17 09:30:33 -0400

    Initial file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    read_file("foo.csv")
+}
+
+fn read_file(filename: &str) -> std::io::Result<()> {
+    let f = File::open(filename)?;
+    let mut reader = BufReader::new(f);
+
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+
+    println!("{}", line);
+    Ok(())
 }
