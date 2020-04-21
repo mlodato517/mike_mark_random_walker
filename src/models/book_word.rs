cbd9bc46a1a4a8220cd9598cd9fcc0833fa15108
Mark Lodato <mark@gnar.dog>
2020-04-21 08:49:03 -0400

    Test BookWord model

--- src/models/book_word.rs
+++ src/models/book_word.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct BookWord {
     book_id: String,
     word_id: String,
@@ -15,5 +15,63 @@ impl BookWord {
         ids.iter()
             .map(|id_tuple| Self::new(&id_tuple[0], &id_tuple[1]))
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let book_word = BookWord::new("1", "2");
+        let expected = BookWord {
+            book_id: "1".to_string(),
+            word_id: "2".to_string(),
+        };
+
+        assert_eq!(book_word, expected);
+    }
+
+    #[test]
+    fn from_non_empty_collection() {
+        let ids = vec![
+            vec!["1".to_string(), "2".to_string()],
+            vec!["2".to_string(), "1".to_string()],
+        ];
+        let collection = BookWord::from_collection(ids);
+
+        let expected = vec![
+            BookWord {
+                book_id: "1".to_string(),
+                word_id: "2".to_string(),
+            },
+            BookWord {
+                book_id: "2".to_string(),
+                word_id: "1".to_string(),
+            },
+        ];
+
+        assert_eq!(collection, expected);
+    }
+
+    #[test]
+    fn from_empty_collection() {
+        let ids = vec![];
+        let collection = BookWord::from_collection(ids);
+
+        let expected = vec![];
+
+        assert_eq!(collection, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn from_incorrectly_sized_collection() {
+        let ids = vec![
+            vec!["1".to_string()],
+            vec!["2".to_string(), "1".to_string()],
+        ];
+        BookWord::from_collection(ids);
     }
 }
